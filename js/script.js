553398e449fc3553627466e72ddb8a0d2a11a87f
Rohit Kumar <thisisrohitkumar@outlook.com>
2023-03-31 00:45:37 +0530

    UI-UX almost completed

--- js/script.js
+++ js/script.js
@@ -7,11 +7,11 @@ function close_warning(){
 function open_nav(){
     document.getElementById('hamburger').style.display = 'none';
     document.getElementById('hamburger-close').style.display = 'block';
-    document.getElementById('nav').style.right = '0';
+    document.getElementById('nav').style.transform = 'translateX(0)';
 } 
 
 function close_nav(){
     document.getElementById('hamburger').style.display = 'block';
     document.getElementById('hamburger-close').style.display = 'none';
-    document.getElementById('nav').style.right = '-100%';
+    document.getElementById('nav').style.transform = 'translateX(100%)';
 }
