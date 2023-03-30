5337b81a3dfc194102587290623a608aeb13342e
Rohit Kumar <thisisrohitkumar@outlook.com>
2023-03-30 20:52:40 +0530

    Basic ui-ux completed

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//close warning function controls warning message on desktop devices
+function close_warning(){
+    document.getElementById('warning').style.display = 'none';
+}
+
+//open-close navigation menu
+function open_nav(){
+    document.getElementById('hamburger').style.display = 'none';
+    document.getElementById('hamburger-close').style.display = 'block';
+    document.getElementById('nav').style.right = '0';
+} 
+
+function close_nav(){
+    document.getElementById('hamburger').style.display = 'block';
+    document.getElementById('hamburger-close').style.display = 'none';
+    document.getElementById('nav').style.right = '-100%';
+}
